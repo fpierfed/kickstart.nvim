ec80c769d46bd288c4001ac17bccccf7958ce057
Francesco Pierfederici <fpierfed@gmail.com>
2025-01-27 16:41:48 +0100

    added yetone/avante.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,69 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons' },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+  },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = 'gemini',
+      gemini = {
+        -- model = 'gemini-exp-1206',
+        model = 'gemini-2.0-flash-thinking-exp-01-21',
+        max_tokens = 2000000,
+      },
+      -- provider = 'deepseek',
+      -- vendors = {
+      --   deepseek = {
+      --     __inherited_from = 'openai',
+      --     api_key_name = 'DEEPSEEK_API_KEY',
+      --     endpoint = 'https://api.deepseek.com',
+      --     model = 'deepseek-coder',
+      --   },
+      -- },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }
